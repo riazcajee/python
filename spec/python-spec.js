e5bc68cc97e4a9f42faf89379e5b031dcb06547d
Riaz <riaz.cajee@gmail.com>
2021-10-25 22:44:44 +0200

    `window:run-package-specs`

--- spec/python-spec.js
+++ spec/python-spec.js
@@ -1,1 +1,73 @@
+'use babel';
+
 import Python from '../lib/python';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Python', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('python');
+  });
+
+  describe('when the python:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.python')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'python:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.python')).toExist();
+
+        let pythonElement = workspaceElement.querySelector('.python');
+        expect(pythonElement).toExist();
+
+        let pythonPanel = atom.workspace.panelForItem(pythonElement);
+        expect(pythonPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'python:toggle');
+        expect(pythonPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.python')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'python:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let pythonElement = workspaceElement.querySelector('.python');
+        expect(pythonElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'python:toggle');
+        expect(pythonElement).not.toBeVisible();
+      });
+    });
+  });
+});
